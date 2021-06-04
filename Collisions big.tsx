84871c28c7264621f39ac53779ed2d7526342444
Hugo Averty <hugoaverty@MacBook-Air-de-Hugo.local>
2021-06-04 12:21:25 +0200

    fix ressources

---
+++ Collisions big.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Collisions big" tilewidth="32" tileheight="32" tilecount="85" columns="5">
+ <image source="watileset.png" width="170" height="544"/>
+</tileset>
---
+++ HUGO/Shadow.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Shadow" tilewidth="32" tileheight="32" tilecount="2" columns="1">
+ <image source="Sans titre-2 copie.png" width="32" height="64"/>
+</tileset>
